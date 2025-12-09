907708da532cc49ada62893e5a0c78a23cef2c9a
Lucas Anacleto <lucasanacleto04@gmail.com>
2025-12-09 00:55:57 -0300

    feat: Finalizando construção da interface da screen <Greetings />

--- app/src/ui/screens/Greetings/index.tsx
+++ app/src/ui/screens/Greetings/index.tsx
@@ -1,9 +1,11 @@
-import { ImageBackground } from "react-native";
+import { ImageBackground, TouchableOpacity, View } from "react-native";
 import greetingsBg from "@ui/assets/greetings-bg/image.png";
 import { styles } from "./styles";
 import { Logo } from "@ui/components/Logo";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Button } from "@ui/components/Button";
+import { AppText } from "@ui/components/AppText";
+import { theme } from "@ui/styles/theme";
 
 export function Greetings() {
   return (
@@ -12,9 +14,31 @@ export function Greetings() {
       resizeMode="cover"
       style={styles.container}
     >
-      <SafeAreaView>
+      <SafeAreaView style={styles.content}>
         <Logo />
-        <Button>Criar minha conta</Button>
+        <View style={styles.ctaContainer}>
+          <AppText
+            style={styles.heading}
+            color={theme.colors.white}
+            size="3xl"
+            weight="semiBold"
+          >
+            Controle sua dieta de forma simples
+          </AppText>
+
+          <View style={styles.ctaContent}>
+            <Button>Criar conta</Button>
+
+            <View style={styles.signInContainer}>
+              <AppText color={theme.colors.white}>Já tem uma conta?</AppText>
+              <TouchableOpacity>
+                <AppText color={theme.colors.lime[500]} weight="medium">
+                  Acesse a sua conta
+                </AppText>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
       </SafeAreaView>
     </ImageBackground>
   );
--- app/src/ui/screens/Greetings/styles.ts
+++ app/src/ui/screens/Greetings/styles.ts
@@ -3,7 +3,32 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  content: {
+    flex: 1,
     alignItems: "center",
+    justifyContent: "space-between",
+    paddingVertical: 16,
+  },
+  heading: {
+    letterSpacing: -0.32,
+    textAlign: "center",
+    maxWidth: 311,
+  },
+  ctaContainer: {
+    width: "100%",
+    alignItems: "center",
+  },
+  ctaContent: {
+    padding: 20,
+    marginTop: 24,
+    width: "100%",
+  },
+  signInContainer: {
+    marginTop: 16,
+    flexDirection: "row",
+    gap: 4,
+    paddingVertical: 14,
     justifyContent: "center",
   },
 });
