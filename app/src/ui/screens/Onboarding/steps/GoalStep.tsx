3df74269108a044e291feba8f3f4961e2b07e220
Lucas Anacleto <lucasanacleto04@gmail.com>
2026-03-04 18:15:16 -0300

    fix: Removendo variáveis que não estão sendo utilizadas do código

--- app/src/ui/screens/Onboarding/steps/GoalStep.tsx
+++ app/src/ui/screens/Onboarding/steps/GoalStep.tsx
@@ -24,7 +24,7 @@ export enum Goal {
 }
 
 export function GoalStep() {
-  const { currentStepIndex, nextStep, previousStep } = useOnboarding();
+  const { nextStep } = useOnboarding();
 
   return (
     <Step>
