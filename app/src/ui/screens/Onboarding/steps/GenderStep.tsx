e12e2779752adf1525e9364296e65ad96ac46a40
Lucas Anacleto <lucasanacleto04@gmail.com>
2026-03-04 18:20:16 -0300

    feat: Implementando componente GenderStep

--- app/src/ui/screens/Onboarding/steps/GenderStep.tsx
+++ app/src/ui/screens/Onboarding/steps/GenderStep.tsx
@@ -1,12 +1,55 @@
-import { AppText } from "@ui/components/AppText";
-import { View } from "react-native";
+import { Button } from "@ui/components/Button";
+import {
+  RadioGroup,
+  RadioGroupIcon,
+  RadioGroupItem,
+  RadioGroupLabel,
+} from "@ui/components/RadioGroup";
+import { theme } from "@ui/styles/theme";
+import { ArrowRightIcon } from "lucide-react-native";
+import {
+  Step,
+  StepContent,
+  StepFooter,
+  StepHeader,
+  StepSubtitle,
+  StepTitle,
+} from "../components/Step";
+import { useOnboarding } from "../context/useOnboarding";
+
+export enum Gender {
+  MALE = "MALE",
+  FEMALE = "FEMALE",
+}
 
 export function GenderStep() {
+  const { nextStep } = useOnboarding();
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <AppText size="3xl" weight="semiBold">
-        GenderStep
-      </AppText>
-    </View>
+    <Step>
+      <StepHeader>
+        <StepTitle>Qual é seu gênero?</StepTitle>
+        <StepSubtitle>Seu gênero influencia no tipo da dieta</StepSubtitle>
+      </StepHeader>
+
+      <StepContent>
+        <RadioGroup orientation="horizontal">
+          <RadioGroupItem value={Gender.MALE}>
+            <RadioGroupIcon>🧔🏻</RadioGroupIcon>
+            <RadioGroupLabel>Masculino</RadioGroupLabel>
+          </RadioGroupItem>
+          <RadioGroupItem value={Gender.FEMALE}>
+            <RadioGroupIcon>👩🏻</RadioGroupIcon>
+            <RadioGroupLabel>Feminino</RadioGroupLabel>
+          </RadioGroupItem>
+        </RadioGroup>
+      </StepContent>
+
+      <StepFooter>
+        <Button size="icon" onPress={nextStep}>
+          <ArrowRightIcon size={20} color={theme.colors.black[700]} />
+        </Button>
+      </StepFooter>
+    </Step>
   );
 }
