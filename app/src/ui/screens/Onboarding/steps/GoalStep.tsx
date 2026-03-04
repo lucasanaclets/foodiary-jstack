a7f51981adfbb0ecd061b1155f62d6e67f230d5a
Lucas Anacleto <lucasanacleto04@gmail.com>
2026-03-04 18:13:16 -0300

    feat: Implementando componente GoalStep a partir dos componentes criados usando composition pattern

--- app/src/ui/screens/Onboarding/steps/GoalStep.tsx
+++ app/src/ui/screens/Onboarding/steps/GoalStep.tsx
@@ -1,20 +1,60 @@
-import { AppText } from "@ui/components/AppText";
 import { Button } from "@ui/components/Button";
-import { View } from "react-native";
+import {
+  RadioGroup,
+  RadioGroupIcon,
+  RadioGroupItem,
+  RadioGroupLabel,
+} from "@ui/components/RadioGroup";
+import { theme } from "@ui/styles/theme";
+import { ArrowRightIcon } from "lucide-react-native";
+import {
+  Step,
+  StepContent,
+  StepFooter,
+  StepHeader,
+  StepSubtitle,
+  StepTitle,
+} from "../components/Step";
 import { useOnboarding } from "../context/useOnboarding";
+
+export enum Goal {
+  LOSE = "LOSE",
+  MAINTAIN = "MAINTAIN",
+  GAIN = "GAIN",
+}
 
 export function GoalStep() {
   const { currentStepIndex, nextStep, previousStep } = useOnboarding();
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <AppText size="3xl" weight="semiBold">
-        GoalStep
-      </AppText>
-      <View>
-        <Button onPress={previousStep}>Voltar</Button>
-        <AppText>{currentStepIndex}</AppText>
-        <Button onPress={nextStep}>Avançar</Button>
-      </View>
-    </View>
+    <Step>
+      <StepHeader>
+        <StepTitle>Qual é o seu objetivo?</StepTitle>
+        <StepSubtitle>O que você pretende alcançar com a dieta?</StepSubtitle>
+      </StepHeader>
+
+      <StepContent>
+        <RadioGroup>
+          <RadioGroupItem value={Goal.LOSE}>
+            <RadioGroupIcon>🥦</RadioGroupIcon>
+            <RadioGroupLabel>Perder peso</RadioGroupLabel>
+          </RadioGroupItem>
+          <RadioGroupItem value={Goal.MAINTAIN}>
+            <RadioGroupIcon>🍍</RadioGroupIcon>
+            <RadioGroupLabel>Manter peso</RadioGroupLabel>
+          </RadioGroupItem>
+          <RadioGroupItem value={Goal.GAIN}>
+            <RadioGroupIcon>🥩</RadioGroupIcon>
+            <RadioGroupLabel>Ganhar peso</RadioGroupLabel>
+          </RadioGroupItem>
+        </RadioGroup>
+      </StepContent>
+
+      <StepFooter>
+        <Button size="icon" onPress={nextStep}>
+          <ArrowRightIcon size={20} color={theme.colors.black[700]} />
+        </Button>
+      </StepFooter>
+    </Step>
   );
 }
